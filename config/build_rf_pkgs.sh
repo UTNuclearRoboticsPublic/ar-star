122439115eb903999fb773ef2d8014bf981bec27
frank-Regal <fregal@utexas.edu>
2023-12-07 01:20:25 -0600

    lets go

--- config/build_rf_pkgs.sh
+++ config/build_rf_pkgs.sh
@@ -2,8 +2,9 @@
 echo "Building Robofleet Packages ..."
 
 # setup
-AR_STAR_ROS_PATH=$(rospack find ar_star_ros)
-cd $AR_STAR_ROS_PATH/../config
+PKG_NAME="ar_star_ros"
+PATH_TO_ROS_PKG=$(rospack find $PKG_NAME)
+cd $PATH_TO_ROS_PKG/../config
 RF_PKG_FILE="rf_pkgs.txt"
 RF_PKG_BUILD_FILE=".rf_build_pkgs.txt"
 
@@ -12,13 +13,13 @@ cp "$RF_PKG_FILE" "$RF_PKG_BUILD_FILE"
 sed -i 's/$/_robofleet/' "$RF_PKG_BUILD_FILE"
 
 # generate and build
-cd $AR_STAR_ROS_PATH/../..
+cd $PATH_TO_ROS_PKG/../..
 rm -rf robofleet_pkgs
-rosrun robofleet_client generate_plugin_pkg.py -o ./robofleet_pkgs -w $(cat $AR_STAR_ROS_PATH/../config/$RF_PKG_FILE)
+rosrun robofleet_client generate_plugin_pkg.py -o ./robofleet_pkgs -w $(cat $PATH_TO_ROS_PKG/../config/$RF_PKG_FILE)
 cd ..
-catkin build $(cat $AR_STAR_ROS_PATH/../config/$RF_PKG_BUILD_FILE)
+catkin build $(cat $PATH_TO_ROS_PKG/../config/$RF_PKG_BUILD_FILE)
 
 # clean up
-cd $AR_STAR_ROS_PATH/../config
+cd $PATH_TO_ROS_PKG/../config
 rm $RF_PKG_BUILD_FILE
-source $AR_STAR_ROS_PATH/../../../devel/setup.bash
+source $PATH_TO_ROS_PKG/../../../devel/setup.bash
